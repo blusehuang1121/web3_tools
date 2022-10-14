571c1bfa588004800773da4e28a825eff769cd22
blusehuang <8532857@qq.com>
2022-10-14 16:14:18 +0800

    fix gas problem on bsc

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 import xen_manager as xen
 import wallet_manager as wal
 
+current_network = xen.Network.eth
 
 def batch_claim_rank():
-    x = xen.XenManager(xen.Network.eth)
+    x = xen.XenManager(current_network)
     print('begin to batch claim rank')
     x.batch_claim_rank('wallets.csv', 300)
 
 
 def batch_claim_mint_reward_and_share():
-    x = xen.XenManager(xen.Network.eth)
+    x = xen.XenManager(current_network)
     print('begin to batch claim mint reward and share')
     x.batch_claim_mint_reward_and_share('wallets.csv', '归集地址')
 
 
 def update_all_mints():
-    x = xen.XenManager(xen.Network.eth)
-    x.update_all_mints('wallets.csv', 'xen_mint_data.csv')
+    x = xen.XenManager(current_network)
+    x.update_all_mints('wallets.csv', f'xen_mint_data_{x._current_network}.csv')
+
+
+def batch_get_balance():
+    x = xen.XenManager(current_network)
+    x.batch_get_balance('wallets.csv')
 
 
 def batch_transfer():
-    from_wallet = [0, '转账地址', 'private_key']
+    from_wallet = [0, '转账地址', '私钥']
     x = xen.XenManager(xen.Network.eth)
-    x.transfer_money(0.004, from_wallet, 'wallets.csv')
+    x.transfer_money(0.001, from_wallet, 'wallets.csv')
 
 
 if __name__ == '__main__':
     print('Demo batch functions for XEN')
+    current_network = xen.Network.bsc
     # 更新全部钱包的mint信息
     # update_all_mints()
 
+    # 批量获得钱包余额
+    # batch_get_balance()
+
     # 批量转账给小钱包
-    # batch_transfer()
+    #batch_transfer()
 
     # 批量mint
     # batch_claim_rank()
